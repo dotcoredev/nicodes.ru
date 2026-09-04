1d27a327ba525b8712d380d6fe657af8887fb88f
dotcoredev <dotcoredev@gmail.com>
2026-09-04 10:54:56 +0500

    feat: update next.config.ts to include standalone output and tracing includes

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	/* config options here */
 	reactCompiler: true,
+	output: "standalone",
+	outputFileTracingIncludes: {
+		"/*": ["./node_modules/@swc/helpers/**/*"],
+	},
 };
 
 export default nextConfig;
